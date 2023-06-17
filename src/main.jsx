90d0b89094da6a1011d3c5b66ff390a9658ddccc
Sakib Hossain <115476707+SakibHossain48@users.noreply.github.com>
2023-06-17 17:47:04 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<div className='max-w-7xl mx-auto'>l
+	<div className='max-w-7xl mx-auto'>
 		<React.StrictMode>
 			<AuthProvider>
 				<QueryClientProvider client={queryClient}>
