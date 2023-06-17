9a00efd275603586c53a1ac664abf240e64a0bef
Sakib Hossain <115476707+SakibHossain48@users.noreply.github.com>
2023-06-17 17:47:25 +0600

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 	appId: import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
+// Initialize Firebase//
 const app = initializeApp(firebaseConfig);
 
 export default app;
